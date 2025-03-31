12ad27c9d74a5e2e6ad21920a6455ca764c75ce0
YuriCDD2025 <YuriCDD@outlook.com>
2025-03-31 17:11:07 -0300

    alteração de imparcialidade e organização através de variáveis para o usuário antes sport e santa depois time1 e time2

--- exe 6.py
+++ exe 6.py
@@ -1,8 +1,10 @@
-Sport = int(input("Digite a quantidade de gol's do Sport na partida: "))
-santa_cruz = int(input("Digite a quantidade de gol's do Santa na partida: "))
-if Sport > santa_cruz:
-    print(f"sport vence santa por {Sport}x{santa_cruz}")
-elif Sport == santa_cruz:
-    print(f"partida entre Sport e santa empata por {Sport}X{santa_cruz}")
+time1=input("Digite o nome do seu time: ")
+time2=input("Digite o nome do seu time: ")
+goltime1=int(input(f"Digite a quantidade de gol's do {time1} na partida: "))
+goltime2=int(input(f"Digite a quantidade de gol's do {time2} na partida: "))
+if time1 > time2:
+    print(f"{time1} vence {time2} por {goltime1}x{goltime2}")
+elif time1 == time2:
+    print(f"partida entre {time1} e {time2} empata  por {goltime1}x{goltime2}")
 else:
-    print(f"Sport perde por {Sport}x{santa_cruz} ")
+    print(f"{time1} perde para o {time2} com o placar de {goltime1}x{goltime2} ")
