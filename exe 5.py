263185472ec960027ee7d34a15b92f7b07ed82c8
YuriCDD2025 <YuriCDD@outlook.com>
2025-03-31 16:20:44 -0300

    alteração no nome do exe4 e o exe5 feito com a média do aluno

---
+++ exe 5.py
@@ -0,0 +1,10 @@
+print("consulte a sua média anual !!!!")
+aluno=input("Digite o seu nome: ")
+nota1=float(input("Digite a sua primeira nota semestre: "))
+nota2=float(input("Digite a sua segunda nota do semestre: "))
+nota3=float(input("Digite a sua terceira nota do semestre: "))
+media=(nota1+nota2+nota3)/3
+if media >= 7:
+    print(f"O aluno {aluno} foi a provado com a média {media} ao final do semestre")
+else:
+    print(f"O aluno {aluno} foi a reprovado com a média {media} ao final do semestre")
