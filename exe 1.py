d245dea69f22e6e968bcbc061ef79e20fc324298
YuriCDD2025 <YuriCDD@outlook.com>
2025-03-31 14:16:47 -0300

    atividade1 coleta nome idade e saláio

---
+++ exe 1.py
@@ -0,0 +1,4 @@
+Pessoa_espec=input("Digite seu nome completo: ")
+idade_pess=int(input("Digite a sua idade: "))
+salario_pes=float(input("Digite o seu salário mensal: "))
+print(f"{Pessoa_espec} tem {idade_pess} anos de idade e ganha {salario_pes} R$ mensalmente como progamador.")
