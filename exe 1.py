b694bbf901f24b1ab219350cb574eb5c44d3da58
Yuriportf <ysoli7777@gmail.com>
2025-04-02 17:03:33 -0300

    o pior código de todos os tempos

--- exe 1.py
+++ exe 1.py
@@ -1,7 +1,8 @@
 Pessoa_espec=input("Digite seu nome: ")
 idade_pess=int(input("Digite a sua idade: "))
 salario_pes=float(input("Digite o seu salário mensal: "))
-almentoporc=float(salario_pes*20)/100
+PORFCENTAGEMDESEJADA=float(input("Digite a quantidade desejada: "))
+almentoporc=float(salario_pes*PORFCENTAGEMDESEJADA)/100
 salariofinal=almentoporc+salario_pes
 print(f"{Pessoa_espec} tem {idade_pess} anos de idade e ganha {salario_pes} R$ mensalmente como progamador,\n",
 f"Recentemente {Pessoa_espec} recebeu um almento de {almentoporc} R$ \n",
---
+++ exe9.py
@@ -0,0 +1,16 @@
+
+hora=3
+mint=45
+hora2=12
+minuto2=20
+somahora=hora+hora2
+somamin=mint+minuto2
+if somamin >=60:
+   somahorat = somahora+1
+if somamin >= 60:
+    somamin - 5
+print(somahorat1)
+
+
+
+
